066bc16ffac65d1f328a8ab994f48b7d9440e096
igcodeui <pwdecipher@gmail.com>
2026-09-15 10:21:37 -0700

    Update apc-office-sync.js

--- apc-office-sync.js
+++ apc-office-sync.js
@@ -5,7 +5,7 @@
    ============================================================ */
 (() => {
   'use strict';
-  const OFFICE_API = 'https://script.google.com/macros/s/AKfycbx-3fn-3ByKw2Ef5fYJ0ulY-Bp4TYPit7fMBcsdDmKmtaPsyNZ7UzWvKuSrpqU5Y6V6/exec';
+  const OFFICE_API = 'https:script.google.com/macros/s/AKfycbzp2ys7EwU_wiWHNAISRaIBIWeddE4CxzQv-r4aivvfKGGMpNc21JZa5yScbI4ePwDy4Q/exec';
   const RR_API = 'https://script.google.com/macros/s/AKfycbyMXt3sSNYLvpL4ZukswXlaIi1kXI3X88ohn163h-QGJk4MmOBN82LfrbmDhcYNSrNG/exec';
   const POLL_MS = 30000;
   const NAME_TO_PC = {'marnelie':'PC-1','selyn':'PC-2','edna':'PC-3','angelo':'PC-4','karen':'PC-5','zara':'PC-6','zarah':'PC-6','gia':'PC-7','cherylyn':'PC-8','arc':'PC-10','frank':'PC-11','minjubail':'PC-13','jd':'PC-14','rea':'PC-15','jhoana':'PC-16'};
